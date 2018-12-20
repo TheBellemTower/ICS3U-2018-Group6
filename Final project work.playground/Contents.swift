2e1eba3d04d14416e893b22e2addfb0898688d1d
Ethan <ethan.bellem@ocsbstudent.ca>
2018-12-20 13:44:55 -0500

    Added in new comment

---
+++ Final project work.playground/Contents.swift
@@ -0,0 +1,140 @@
+
+
+// Created on: Dec 2018
+// Created by: Ethan Bellem, Amin Zeina
+// Created for: ICS3U
+// This program is the ICS3U final project
+// some change
+
+// this will be commented out when code moved to Xcode
+// test
+import PlaygroundSupport
+
+
+import SpriteKit
+
+class SplashScene: SKScene {
+    // local variables to this scene
+    let splashSceneBackground = SKSpriteNode(imageNamed: "splashSceneImage.png")
+    let moveToNextSceneDelay = SKAction.wait(forDuration: 2.0)
+    
+    override func didMove(to view: SKView) {
+        // this is run when the scene loads
+        
+        /* Setup your scene here */
+        self.backgroundColor = SKColor(red: 0.0, green:0.0, blue:0.0, alpha: 1.0)
+        
+        splashSceneBackground.name = "splash scene background"
+        splashSceneBackground.position = CGPoint(x: frame.midX, y: frame.midY)
+        splashSceneBackground.size = CGSize(width: frame.size.width, height: frame.size.height)
+        self.addChild(splashSceneBackground)
+        
+        splashSceneBackground.run(moveToNextSceneDelay) {
+            //run code here after 3 secs
+            
+            self.splashSceneBackground.removeAllActions()
+            let loadingScene = LoadingScene(size: self.size)
+            self.view?.presentScene(loadingScene)
+        }
+    }
+    
+    override func  update(_ currentTime: TimeInterval) {
+        //
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        //
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //when the screen is touched, it moves to the main menu
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //
+        
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //
+    }
+}
+
+class LoadingScene: SKScene, SKPhysicsContactDelegate {
+    // local variables to this scene
+    let loadingScreenBackground = SKSpriteNode(imageNamed: "IMG_2340.JPG")
+    let moveToNextSceneDelay = SKAction.wait(forDuration: 2.0)
+    
+    override func didMove(to view: SKView) {
+        // this is run when the scene loads
+        
+        /* Setup your scene here */
+        self.backgroundColor = SKColor(red: 1.0, green: 1.0, blue:1.0, alpha: 1.0)
+        
+        loadingScreenBackground.name = "loading scene background"
+        loadingScreenBackground.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+
+loadingScreenBackground.size = CGSize(width: frame.size.width, height: frame.size.height)
+        self.addChild(loadingScreenBackground)
+        
+        loadingScreenBackground.run(moveToNextSceneDelay) {
+            //run code here after 3 secs
+            self.loadingScreenBackground.removeAllActions()
+            let splashScene = SplashScene(size: self.size)
+            self.view?.presentScene(splashScene)
+        }
+    }
+    
+    override func  update(_ currentTime: TimeInterval) {
+        //
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        //
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // when the start button is touched, it moves to the main game
+        
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //
+        
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //
+        
+        
+    }
+}
+
+// this will be commented out when code moved to Xcode
+
+// set the frame to be the size for your iPad
+
+let screenSize = UIScreen.main.bounds
+let screenWidth = screenSize.width
+let screenHeight = screenSize.height
+let frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+
+let scene = SplashScene(size: frame.size)
+scene.scaleMode = SKSceneScaleMode.resizeFill
+
+
+let skView = SKView(frame: frame)
+skView.showsFPS = true
+skView.showsNodeCount = true
+skView.presentScene(scene)
+
+PlaygroundPage.current.liveView = skView
